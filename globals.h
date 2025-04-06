a4f1e60357c23d03cb7279bfdf48298906413d87
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-04-06 14:52:58 +0000

    feat: add global.h to store and access game state globally

---
+++ globals.h
@@ -0,0 +1,24 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include "themes.h"
+
+// game state
+typedef enum { MENU, INFO, SETTINGS, GAME } GAME_STATE;
+
+typedef struct {
+	GAME_STATE state;
+	THEME_ID theme;
+} Damapp;
+
+static inline Damapp *GetGameStruct()
+{
+	static Damapp game = {
+		.state = MENU,
+		.theme = THEME_WOOD,
+	};
+
+	return &game;
+}
+
+#endif
