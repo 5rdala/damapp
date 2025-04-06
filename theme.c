d76bb8fa38caecb3e45a826860977e9a9e8ddb51
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-04-06 14:54:21 +0000

    refactor(theme): move theme data and access function to theme.h

--- theme.c
+++
@@ -1,8 +0,0 @@
-#include "themes.h"
-
-THEME_ID current_theme = THEME_WOOD;
-
-Theme themes[] = {
-	{(Color){230, 204, 167, 255}, (Color){55, 38, 25, 255}}, // wood theme
-	{(Color){255, 255, 255, 255}, (Color){0, 0, 0, 255}}, // black and white theme
-};
--- themes.h
+++ themes.h
@@ -2,6 +2,7 @@
 #define THEMES_H
 
 #include <raylib.h>
+#include <stdlib.h>
 
 typedef enum { THEME_WOOD, THEME_BLACK_AND_WHITE } THEME_ID;
 
@@ -10,7 +11,17 @@ typedef struct {
 	Color dark;
 } Theme;
 
-extern THEME_ID current_theme;
-extern Theme themes[];
+static inline Theme *GetThemeById(THEME_ID id)
+{
+	static Theme themes[] = {
+		{(Color){230, 204, 167, 255}, (Color){55, 38, 25, 255}}, // wood theme
+		{(Color){255, 255, 255, 255}, (Color){0, 0, 0, 255}}, // black and white theme
+	};
+
+	if (id < 0 || id > 1)
+		return NULL;
+
+	return &themes[id];
+}
 
 #endif // !THEMES_H
