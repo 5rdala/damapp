3c442bc0358dd7e23070ccc0ad06e33030c87592
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-04-06 00:03:52 +0000

    feat(game): implement game board initialization and drawing logic
    - define static 2D array to hold cells structures
    
    - implement InitGameBoard() to initialize the board
    
    - implement DrawGameBoard() to draw the game board
    
    - implement GetCells() to provide access to the cells 2d array

---
+++ board.c
@@ -0,0 +1,44 @@
+#include <raylib.h>
+
+#include "board.h"
+
+static Cell cells[DEFAULT_BOARD_SIZE][DEFAULT_BOARD_SIZE];
+
+void InitGameBoard(void)
+{
+	for (int y = 0; y < DEFAULT_BOARD_SIZE; y++) {
+		for (int x = 0; x < DEFAULT_BOARD_SIZE; x++) {
+			if ((x + y) % 2 == 0) { // only place pieces on dark squares
+				if (y <= 2) {
+					cells[y][x].state = BLACK_PIECE;
+				} else if (y >= DEFAULT_BOARD_SIZE - 3) {
+					cells[y][x].state = WHITE_PIECE;
+				} else {
+					cells[y][x].state = EMPTY_CELL;
+				}
+			} else {
+				cells[y][x].state = EMPTY_CELL;
+			}
+		}
+	}
+}
+
+void DrawGameBoard(void)
+{
+	int cellSize = GetScreenWidth() / DEFAULT_BOARD_SIZE;
+
+	for (int y = 0; y < DEFAULT_BOARD_SIZE; y++) {
+		for (int x = 0; x < DEFAULT_BOARD_SIZE; x++) {
+			Color color = (x + y) % 2 == 0 ? BLACK : WHITE;
+			DrawRectangle(x * cellSize, (y * cellSize), cellSize, cellSize, color);
+			cells[y][x].size = cellSize;
+			cells[y][x].posX = x * cellSize;
+			cells[y][x].posY = (y * cellSize);
+		}
+	}
+}
+
+Cell *GetCells(void)
+{
+	return (Cell *)&cells[0][0];
+}
