b2ee4ac6032763c1fdbf53a9629fb5f10fc28d9a
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-04-06 11:21:34 +0000

    feat(themes): implement theme data in themes.c

---
+++ theme.c
@@ -0,0 +1,8 @@
+#include "themes.h"
+
+THEME_ID current_theme = THEME_WOOD;
+
+Theme themes[] = {
+	{(Color){230, 204, 167, 255}, (Color){55, 38, 25, 255}}, // wood theme
+	{(Color){255, 255, 255, 255}, (Color){0, 0, 0, 255}}, // black and white theme
+};
