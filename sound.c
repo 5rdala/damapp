aab9659efeba0bbade45b32ad72ce0820a5e6476
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-04-06 02:07:06 +0000

    feat(sounds): implement LoadSounds() to load sounds and UnLoadSounds() to unload them and GetClickSound()

---
+++ sound.c
@@ -0,0 +1,24 @@
+#include <raylib.h>
+
+#include "sounds/button_click.h"
+
+static Sound click_sound = {};
+
+void LoadSounds(void)
+{
+	// button click sound
+	Wave click_wave = LoadWaveFromMemory(".wav", button_click_wav, button_click_wav_len);
+	click_sound = LoadSoundFromWave(click_wave);
+	UnloadWave(click_wave);
+}
+
+void UnLoadSounds(void)
+{
+	// unload button click sound
+	UnloadSound(click_sound);
+}
+
+Sound *GetClickSound(void)
+{
+	return &click_sound;
+}
